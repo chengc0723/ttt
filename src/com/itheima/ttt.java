f19c7ef712e5474d4c0d10c8d1326e4147190cd0
chengc <chengc0723@163.com>
2018-10-29 19:19:30 +0800

    ttt

---
+++ src/com/itheima/ttt.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+public class ttt {
+    public static void main(String[] args) {
+
+        System.out.println("fkdsjalf");
+        System.out.println("fkdsjalf");
+        System.out.println("fkdsjalf");
+        System.out.println("fkdsjalf");
+        System.out.println("fkdsjalf");
+
+    }
+}
